c9b0c1cd987800b2157d32eee36124f2817044e4
Noah Stinson <noahstjohn@ymail.com>
2021-01-22 13:59:37 -0600

    Added some theme data / route for move screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:creative1/start_screen.dart';
+import 'package:creative1/screens/move_screen.dart';
+import 'package:creative1/screens/start_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,10 +13,20 @@ class Creative1App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
+        iconTheme: IconThemeData(
+          size: 50.0,
+        ),
+        buttonTheme: ButtonThemeData(
+          minWidth: 50.0,
+          height: 50.0,
+          buttonColor: Colors.grey[850],
+          disabledColor: Colors.grey[350],
+        ),
       ),
       initialRoute: StartScreen.routeName,
       routes: {
         StartScreen.routeName: (context) => StartScreen(),
+        MoveScreen.routeName: (context) => MoveScreen(),
       },
     );
   }
