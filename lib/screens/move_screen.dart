5d1a399fe1c64e8bf287b07495c12694e38bf30d
Noah Stinson <noahstjohn@ymail.com>
2021-01-22 14:47:00 -0600

    added directional buttons graphics

--- lib/screens/move_screen.dart
+++ lib/screens/move_screen.dart
@@ -17,20 +17,89 @@ class MoveScreen extends StatelessWidget {
                 Container(
                   width: 250.0,
                   height: 250.0,
-                  color: Colors.grey[400],
-                ),
-                Column(
-                  children: [
-                    RaisedButton.icon(
-                      icon: Icon(
-                        Icons.arrow_upward_rounded,
-                        size: 35.0,
-                      ),
-                      onPressed: () => print('Up'),
-                      label: Text('North'),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[400],
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(35),
                     ),
-                  ],
-                )
+                  ),
+                ),
+                Positioned(
+                  left: 90.0,
+                  top: 15.0,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    onPressed: () => print('Up'),
+                    color: Colors.grey[850],
+                    disabledColor: Colors.grey[500],
+                    minWidth: 60.0,
+                    height: 65.0,
+                    child: Icon(
+                      Icons.arrow_upward_rounded,
+                      size: 35.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 15.0,
+                  top: 93.0,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    onPressed: null,
+                    color: Colors.grey[850],
+                    disabledColor: Colors.grey[500],
+                    minWidth: 60.0,
+                    height: 65.0,
+                    child: Icon(
+                      Icons.arrow_back_rounded,
+                      size: 35.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 90.0,
+                  top: 170.0,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    onPressed: null,
+                    color: Colors.grey[850],
+                    disabledColor: Colors.grey[500],
+                    minWidth: 60.0,
+                    height: 65.0,
+                    child: Icon(
+                      Icons.arrow_downward_rounded,
+                      size: 35.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 165.0,
+                  top: 93.0,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    onPressed: null,
+                    color: Colors.grey[850],
+                    disabledColor: Colors.grey[500],
+                    minWidth: 60.0,
+                    height: 65.0,
+                    child: Icon(
+                      Icons.arrow_forward_rounded,
+                      size: 35.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
