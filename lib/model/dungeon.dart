9877d0af4fc5ab2b5436ce6b2450dbfcc67bcc55
Noah Stinson <noahstjohn@ymail.com>
2021-01-25 16:51:08 -0600

    added node text descriptions

--- lib/model/dungeon.dart
+++ lib/model/dungeon.dart
@@ -78,7 +78,7 @@ class Dungeon {
     dungeonMap[0] = DungeonNode(
       nodeID: 0,
       itemID: -1,
-      text: 'Start',
+      text: 'Welcome to the dungeon! There\'s no turning back now...',
       upID: 1,
       rightID: null,
       downID: null,
@@ -88,7 +88,8 @@ class Dungeon {
     dungeonMap[1] = DungeonNode(
       nodeID: 1,
       itemID: -1,
-      text: 'Room 01',
+      text:
+          'Iron bars fall down behind you as you enter this hallway. A long hallway leads North or East, as well as a hallway heading West.',
       upID: 6,
       rightID: 8,
       downID: null,
@@ -98,7 +99,7 @@ class Dungeon {
     dungeonMap[2] = DungeonNode(
       nodeID: 2,
       itemID: -1,
-      text: 'Room 02',
+      text: 'This hallway leads North, East, and South.',
       upID: 3,
       rightID: 1,
       downID: 5,
@@ -108,7 +109,7 @@ class Dungeon {
     dungeonMap[3] = DungeonNode(
       nodeID: 3,
       itemID: -1,
-      text: 'Room 03',
+      text: 'This hallway leads East and South',
       upID: null,
       rightID: 4,
       downID: 2,
@@ -118,7 +119,7 @@ class Dungeon {
     dungeonMap[4] = DungeonNode(
       nodeID: 4,
       itemID: -1,
-      text: 'Room 04 (Dead End)',
+      text: 'Dead End',
       upID: null,
       rightID: null,
       downID: null,
@@ -128,7 +129,8 @@ class Dungeon {
     dungeonMap[5] = DungeonNode(
       nodeID: 5,
       itemID: 4,
-      text: 'Room 05 (Teddy Bear)',
+      text:
+          'You open a door to a large room with nothing but a... Teddy Bear? I mean, I guess it could be useful.',
       upID: 2,
       rightID: null,
       downID: null,
@@ -138,7 +140,8 @@ class Dungeon {
     dungeonMap[6] = DungeonNode(
       nodeID: 6,
       itemID: -1,
-      text: 'Room 06',
+      text:
+          'You make it halfway across the hallway, when you notice a hallway heading East as well.',
       upID: 15,
       rightID: 7,
       downID: 1,
@@ -148,7 +151,8 @@ class Dungeon {
     dungeonMap[7] = DungeonNode(
       nodeID: 7,
       itemID: 1,
-      text: 'Room 07 (Point Helmet)',
+      text:
+          'You come across a skeleton in the corner of this hallway sitting criss-cross-apple sauce. He has a point helmet that looks useful. The hallway leads West and South.',
       upID: null,
       rightID: null,
       downID: 8,
@@ -158,7 +162,8 @@ class Dungeon {
     dungeonMap[8] = DungeonNode(
       nodeID: 8,
       itemID: -1,
-      text: 'Room 08',
+      text:
+          'This a part of long hallway that extends West to East. There is also a hallway leading North',
       upID: 7,
       rightID: 9,
       downID: null,
@@ -168,7 +173,8 @@ class Dungeon {
     dungeonMap[9] = DungeonNode(
       nodeID: 9,
       itemID: -1,
-      text: 'Room 9',
+      text:
+          'You reach the corner of a hallway, with a long hallway heading both North and West.',
       upID: 10,
       rightID: null,
       downID: null,
@@ -178,7 +184,8 @@ class Dungeon {
     dungeonMap[10] = DungeonNode(
       nodeID: 10,
       itemID: 2,
-      text: 'Room 10',
+      text:
+          'Along this hallway you notice a potion sitting atop a pedestal. It smells awful, but you take a swig anyway. You feel much stronger!',
       upID: 11,
       rightID: null,
       downID: 9,
@@ -188,7 +195,8 @@ class Dungeon {
     dungeonMap[11] = DungeonNode(
       nodeID: 11,
       itemID: -1,
-      text: 'Room 11',
+      text:
+          'A path extends West and South, but the path North has an air of danger coming from it. I probably shouldn\'t go there unless I\'m ready...',
       upID: 23,
       rightID: null,
       downID: 10,
@@ -198,7 +206,8 @@ class Dungeon {
     dungeonMap[12] = DungeonNode(
       nodeID: 12,
       itemID: -1,
-      text: 'Room 12',
+      text:
+          'The hallway continues from East to West with another hallway extending South',
       upID: null,
       rightID: 11,
       downID: 13,
@@ -208,7 +217,7 @@ class Dungeon {
     dungeonMap[13] = DungeonNode(
       nodeID: 13,
       itemID: -1,
-      text: 'Room 13',
+      text: 'This short hallway has a door on the East wall. The hallway extends North',
       upID: 12,
       rightID: 14,
       downID: null,
@@ -218,7 +227,8 @@ class Dungeon {
     dungeonMap[14] = DungeonNode(
       nodeID: 14,
       itemID: 0,
-      text: 'Room 14',
+      text:
+          'You open the door and find the dungeon\'s armory. You peruse the weapons until you find an amethyst sword that catches your eye.',
       upID: null,
       rightID: null,
       downID: null,
@@ -228,7 +238,7 @@ class Dungeon {
     dungeonMap[15] = DungeonNode(
       nodeID: 15,
       itemID: -1,
-      text: 'Room 15',
+      text: 'The hallway extends East to West with a path heading South.',
       upID: null,
       rightID: 12,
       downID: 6,
@@ -238,7 +248,7 @@ class Dungeon {
     dungeonMap[16] = DungeonNode(
       nodeID: 16,
       itemID: -1,
-      text: 'Room 16',
+      text: 'The hallway comes to an end, with a path heading South and East.',
       upID: null,
       rightID: 15,
       downID: 17,
@@ -248,7 +258,7 @@ class Dungeon {
     dungeonMap[17] = DungeonNode(
       nodeID: 17,
       itemID: -1,
-      text: 'Room 17',
+      text: 'This hallway cuts North and West.',
       upID: 16,
       rightID: null,
       downID: null,
@@ -258,7 +268,7 @@ class Dungeon {
     dungeonMap[18] = DungeonNode(
       nodeID: 18,
       itemID: -1,
-      text: 'Room 18',
+      text: 'This hallway is darker than normal, and leads North and East.',
       upID: 19,
       rightID: 17,
       downID: null,
@@ -268,7 +278,7 @@ class Dungeon {
     dungeonMap[19] = DungeonNode(
       nodeID: 19,
       itemID: -1,
-      text: 'Room 19',
+      text: 'This hallway is pitch black as you travel. You can\'t see anything.',
       upID: 20,
       rightID: null,
       downID: 18,
@@ -278,7 +288,8 @@ class Dungeon {
     dungeonMap[20] = DungeonNode(
       nodeID: 20,
       itemID: -1,
-      text: 'Room 20',
+      text:
+          'You have been walking this hallway for an uncomfortably long time. You\'re unsure what time it is or how long you have been walking.',
       upID: 21,
       rightID: null,
       downID: 19,
@@ -288,7 +299,8 @@ class Dungeon {
     dungeonMap[21] = DungeonNode(
       nodeID: 21,
       itemID: -1,
-      text: 'Room 21',
+      text:
+          'Your mind begins to wander, you begin to lose sense of not just time, but yourself. Continuing could be risky.',
       upID: 22,
       rightID: null,
       downID: 20,
@@ -298,7 +310,8 @@ class Dungeon {
     dungeonMap[22] = DungeonNode(
       nodeID: 22,
       itemID: 3,
-      text: 'Room 22 (Sunglasses)',
+      text:
+          'You see a light at the end with some sunglasses. Oh yeah! These are the Sunglasses of Super Mega Ultimate Unbelievable Inconceivable Destruction. Worth it!',
       upID: null,
       rightID: null,
       downID: 21,
